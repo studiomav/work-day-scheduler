92c69e86be99ac192f99acdc94f3748324cb7748
Michael Virga <dr.mvirga@gmail.com>
2022-10-22 14:46:16 -0700

    added save/load/erase functionality

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,6 +1,6 @@
 //getting
 var timeContainer = $("#time-container");
-var curTimeContainer = $("#curTime");
+var curTimeContainer = $("#cur-day");
 
 var curTime = moment().format("hh:mmA on DD-MM-YYYY");
 var curHour = moment().hour();
@@ -15,7 +15,6 @@ while (h != 0)
     //create the hourly row
     var newRow = document.createElement("div");
     newRow.classList.add("row");
-    newRow.setAttribute("num", i - 9);
 
     //create the hour element within our new row
     var newHour = document.createElement("span");
@@ -24,14 +23,21 @@ while (h != 0)
     newRow.append(newHour);
 
     //create hourly content field
-    var newContent = document.createElement("span");
-    newContent.classList.add("content", "center");
-    newContent.innerHTML = "Lorem Ipsum";
+    var newContent = document.createElement("input");
+    newContent.classList.add("content", "center", "transparent");
+    newContent.setAttribute("num", i - 9);
+    //load from storage
+    var v = localStorage.getItem(`item${i - 9}`);
+    if (v != null)
+    {
+        newContent.value = v;
+    }
     newRow.append(newContent);
 
     //create the save button for each row
-    var newSaveBtn = document.createElement("span");
-    newSaveBtn.classList.add("save-btn", "center")
+    var newSaveBtn = document.createElement("button");
+    newSaveBtn.classList.add("save-btn", "center");
+    newSaveBtn.setAttribute("num", i - 9);
     newSaveBtn.innerHTML = "save";
     newRow.append(newSaveBtn);
 
@@ -49,3 +55,22 @@ while (h != 0)
     if (h > 12) {h = 1; ampm = "pm"};
     if (h == 6) h = 0;
 }
+
+//save listener
+$(".save-btn").click(function()
+{
+    var saveID = this.getAttribute("num");
+    var saveContent = $(`input[num="${saveID}"]`)[0].value;
+    localStorage.setItem(`item${saveID}`, saveContent);
+    console.log(saveContent);
+});
+
+//delete data listener
+$("#erase-btn").click(function()
+{
+    localStorage.clear();
+    $(".content").each(function()
+    {
+        this.value = "";
+    });
+});
